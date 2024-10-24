6ea8b0c61f77d768840bd039700f89aa07bce0ab
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-10-24 07:39:23 +0200

    fix: ntt test

--- whir/src/utils.rs
+++ whir/src/utils.rs
@@ -65,13 +65,16 @@ mod tests {
 
     #[test]
     fn test_evaluations_stack() {
-        use crate::crypto::fields::Field64 as F;
+        // use crate::crypto::fields::Field64 as F;
+        // use p3_field::Field as F;
+        use p3_mersenne_31::Mersenne31 as F;
+        use p3_field::AbstractField;
 
         let num = 256;
         let folding_factor = 3;
         let fold_size = 1 << folding_factor;
         assert_eq!(num % fold_size, 0);
-        let evals: Vec<_> = (0..num as u64).map(F::from).collect();
+        let evals: Vec<_> = (0..num as u64).map(F::from_canonical_u64).collect();
 
         let stacked = stack_evaluations(evals, folding_factor);
         assert_eq!(stacked.len(), num);
@@ -79,7 +82,7 @@ mod tests {
         for (i, fold) in stacked.chunks_exact(fold_size).enumerate() {
             assert_eq!(fold.len(), fold_size);
             for j in 0..fold_size {
-                assert_eq!(fold[j], F::from((i + j * num / fold_size) as u64));
+                assert_eq!(fold[j], F::from_canonical_u64((i + j * num / fold_size) as u64));
             }
         }
     }
